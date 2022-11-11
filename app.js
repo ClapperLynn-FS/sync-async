bf33d378b7d8a35458f47693a913510613fc162a
Lynn Clapper <leclapper@student.fullsail.edu>
2022-11-11 12:14:39 -0500

    Create app.js

---
+++ app.js
@@ -0,0 +1,41 @@
+//var http = require('htt');
+var http = require('http');
+// var myname = functon() {
+var myname = console.log("Here is my IP address");
+
+
+//function callHttpbi() {
+async function callHttpbin() {
+  let promise = new Promise((resolve, reject) => {
+    http.get(
+     'http://httpbin.org/ip',
+     function(response) {
+      var str="";
+      response.setEncoding('utf8');
+      response.on('data', function(data){
+      //str += data;
+        str += data;
+     });
+     response.on('end', function() {
+      var result = JSON.parse(str);
+      myips = result.origin;
+      //resolve()
+      resolve(data)
+     });
+     }
+    );
+});
+
+let result = await promise;
+// result;
+return result;
+}
+function executeAsyncTask(){
+    //const valueA = await callHttpbin()
+  const valueA =  callHttpbin()
+  const valueB = myname();
+  //console.log(valueB+" "+valueA)
+  console.log(valueB+" "+valueA);
+  //missing }
+}
+// Output Here is my IP address 149.24.160.1, 149.24.160.1
